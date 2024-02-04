bb49f641d50f20ab5c9deab8e645fc8920d766d9
sharrmeen <sharmeenk2020@gmail.com>
2024-02-05 01:13:31 +0530

    modified:   news_scrap.py

--- news_scrap.py
+++ news_scrap.py
@@ -1,21 +1,28 @@
-import cowsay
 from requests_html import HTMLSession
 
 session=HTMLSession()
 
 url='https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen'
-
 r=session.get(url)
 r.html.render(sleep=1,scrolldown=1)
 
-articles = r.html.find('article')
+articles = r.html.find('a.gPFEn')
+# print(articles)
+newslist=[]
 
+    
 for item in articles:
-    try:
-        newsitem = item.find('h3',first=True)
-        title = newsitem.text
-        link =  newsitem.absolute_links
-        print(title,link)
-    except:
-        pass
+        
+    title = item.text
+    link = "https://news.google.com" + item.attrs['href']
+    
+    newsarticle = {
+        'title': title,
+        'link': link 
+    }
+    newslist.append(newsarticle)
 
+
+print(len(newslist))
+
+# link=https://news.google.com/home?hl=en-IN&gl=IN&ceid=IN:en
